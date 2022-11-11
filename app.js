16fe9f1731819371fc42622b6c35e7ba97b80fbb
IlariaDeFer <114747915+IlariaDeFer@users.noreply.github.com>
2022-11-11 18:40:52 +0400

    add js

---
+++ app.js
@@ -0,0 +1,10 @@
+const myName = 'Ana';
+const myLastName = 'Chimakadze';
+const yearOfBirth = 1997;
+const currentYear = 2022;
+
+const text = `My name is ${myName} and my last name is ${myLastName}. I am ${2022-1997} years old.`;
+const text2 = 'My name is' + " " + myName + " " + 'and my last name is' + " " + myLastName + '. I am' + " " +(2022-1997) + " " + 'years old.';
+
+console.log(text);
+console.log(text2);
